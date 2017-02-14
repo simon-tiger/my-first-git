2bc338084d2cd87d5ac30c985efa5f6c70c3c9a9
simontiger <monajune@gmail.com>
2017-02-14 04:18:16 -0800

    Adding an example node.js server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+var server = app.listen(process.env.PORT || 3000, listen);
+
+function listen() {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at http://' + host + ':' + port);
+}
